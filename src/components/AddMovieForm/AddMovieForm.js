5e6b50e16e9e569ea08a32d7292c2d37eabfa36f
mkubibaev <mkubibaev@gmail.com>
2019-01-18 14:32:35 +0600

    added main container and components

---
+++ src/components/AddMovieForm/AddMovieForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddMovieForm = props => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default AddMovieForm;
---
+++ src/containers/Watchlist/Watchlist.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Watchlist extends Component {
+    render() {
+        return (
+            <div className="container">
+
+            </div>
+        );
+    }
+}
+
+export default Watchlist;
